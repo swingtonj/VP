033d509fc4148a60e2bce4a4555e23b6c4e64752
swington <swingtonarul@angleritech.com>
2018-09-07 17:01:43 +0530

    Intial

---
+++ VP/App_Start/BundleConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace VP
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                "~/Assets/js/jquery-3.3.1.min.js",
+                "~/Assets/js/bootstrap.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Assets/css/bootstrap.min.css",
+                "~/Assets/css/font-awesome.min.css",
+                      "~/Assets/css/master.min.css"));
+        }
+    }
+}
