6ad7b44e5ff40527f6a56889ed2444dcd92c5525
swington <swingtonarul@angleritech.com>
2018-10-05 19:34:48 +0530

    tt

--- VP/Controllers/HomeController.cs
+++ VP/Controllers/HomeController.cs
@@ -3,33 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VP.Models;
 
 namespace VP.Controllers
 {
     public class HomeController : Controller
     {
+        CP_Analytics_predictorEntities _context = new CP_Analytics_predictorEntities();
         public ActionResult Index()
         {
             return View();
         }
-
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult Index(Login m_Login)
         {
-            ViewBag.Message = "Your application description page.";
+            var o_validate = _context.Tbl_T_UserManagement.Where(x => x.User_name == m_Login.L_Username && x.Password == m_Login.L_Password && x.Status == true).FirstOrDefault();
+            if (o_validate != null)
+            {
+                Session["Username"] = o_validate.User_name;
+                Session["Userid"] = o_validate.User_Id;
+                Session["IsAdmin"] = o_validate.Is_Admin;
+                return RedirectToAction("index","specify");
+            }
+            else
+            {
 
+            }
             return View();
         }
-
-        public ActionResult Contact()
+        [HttpPost]
+        public ActionResult Register(Login m_Login)
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-        public ActionResult Benefits()
-        {
-            ViewBag.Message = "Your contact page.";
-
+            Tbl_T_UserManagement insert_user = new Tbl_T_UserManagement();
+            insert_user.Email_id = m_Login.R_Email;
+            insert_user.Mobile_no = m_Login.R_Mobile;
+            insert_user.Organisation_name = m_Login.R_Organisation_Name;
+            insert_user.Password = m_Login.R_Passsword;
+            insert_user.Registered_on = DateTime.Now;
+            insert_user.Status = true;
+            insert_user.User_name = m_Login.R_User_Name;
+            _context.Tbl_T_UserManagement.Add(insert_user);
+            _context.SaveChanges();
+            var user_id = insert_user.User_Id;
+           // _context.SP_Registration("","","","","");
             return View();
         }
     }
---
+++ VP/Models/Login.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace VP.Models
+{
+    public class Login
+    {
+        [Required(ErrorMessage ="Please Enter Username")]
+        public string L_Username { get; set; }
+
+        [Required(ErrorMessage = "Please Enter password")]
+        public string L_Password { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Username")]
+        public string R_Organisation_Name { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Username")]
+        public string R_User_Name { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Username")]
+        public string R_Email { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Username")]
+        public string R_Passsword { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Username")]
+        public string R_Mobile { get; set; }
+        
+    }
+}
