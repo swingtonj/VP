a1e63a24babf33e0d7905b64c59f1d984aa2041c
swington <swingtonarul@angleritech.com>
2018-10-09 16:30:27 +0530

    tt

--- VP/Controllers/SpecifyController.cs
+++ VP/Controllers/SpecifyController.cs
@@ -32,36 +32,94 @@ namespace VP.Controllers
         [HttpPost]
         public ActionResult Result_report(string industry, string businessimperative, string amount, string analytics)
         {
+            try
+            {
+                if (Convert.ToString(Session["FileId"]) != "")
+                    System.IO.File.Delete(Convert.ToString(Session["FileId"]));
+            }catch(Exception ee)
+            {
+
+            }
             Session["FileId"] = DateTime.Now.Ticks.ToString();
-            //System.IO.File.Copy(Server.MapPath("~/Documents/IGFCalculatorImplementationV310-19-2016.xlsm"), Server.MapPath("~/Documents/IGF"+Convert.ToString(Session["FileId"]) + ".xlsm"));
             System.IO.File.Copy(Server.MapPath("~/Documents/z14.xlsm"), Server.MapPath("~/Documents/z14" + Convert.ToString(Session["FileId"]) + ".xlsm"));
-            ////String excelFile = Convert.ToString(Server.MapPath("~/Documents/IGFCalculatorImplementationV1.xlsx"));
             String excelFile = Convert.ToString(Server.MapPath("~/Documents/z14" + Convert.ToString(Session["FileId"]) + ".xlsm"));
-            //Workbook workbook = new Workbook();
-            //workbook.LoadFromFile(excelFile);
-            ////The first sheet of excel file
-            //// Worksheet sheet = workbook.Worksheets["Input"];
-            ////sheet.Range["F10"].Text = industry;
-            ////sheet.Range["F12"].Text = businessimperative;
-            ////sheet.Range["F14"].NumberValue = Convert.ToUInt64(amount);
-            ////sheet.Range["F16"].Text = analytics;
-            ////workbook.CalculateAllValue();
-            //workbook.SaveToFile(excelFile);
-
-
             FileInfo file = new FileInfo(excelFile);
             using (ExcelPackage excelPackage = new ExcelPackage(file))
             {
                 ExcelWorkbook excelWorkBook = excelPackage.Workbook;
                 ExcelWorksheet excelWorksheet = excelWorkBook.Worksheets["Input"];
-                excelWorksheet.Cells[10,6].Value = industry;
+                excelWorksheet.Cells[10, 6].Value = industry;
                 excelWorksheet.Cells[12, 6].Value = businessimperative;
                 excelWorksheet.Cells[14, 6].Value = Convert.ToDouble(amount);
                 excelWorksheet.Cells[16, 6].Value = analytics;
 
                 excelPackage.Save();
             }
-            return RedirectToAction("Index", "Specify", new { input = "#step-5" });
+            using (ExcelPackage excelPackage = new ExcelPackage(file))
+            {
+                ExcelWorkbook excelWorkBook = excelPackage.Workbook;
+                ExcelWorksheet excelWorksheet = excelWorkBook.Worksheets["Results"];
+
+                var z14_roi = excelWorksheet.Cells[5, 4].Value;
+                var z14_pp = excelWorksheet.Cells[9, 4].Value;
+                var z14_sa = excelWorksheet.Cells[14, 4].Value;
+                var z14_sy = excelWorksheet.Cells[15, 4].Value;
+                var z14_oc = excelWorksheet.Cells[16, 4].Value;
+                var z14_cp = excelWorksheet.Cells[18, 4].Value;
+                var z14_itp = excelWorksheet.Cells[19, 4].Value;
+                var z14_op = excelWorksheet.Cells[20, 4].Value;
+                var z14_ir = excelWorksheet.Cells[22, 4].Value;
+                var z14_ftv = excelWorksheet.Cells[23, 4].Value;
+                var z14_te = excelWorksheet.Cells[24, 4].Value;
+                var z14_es = excelWorksheet.Cells[26, 4].Value;
+                var z14_gc = excelWorksheet.Cells[27, 4].Value;
+                var z14_rd = excelWorksheet.Cells[28, 4].Value;
+
+                var x86_roi = excelWorksheet.Cells[5, 5].Value;
+                var x86_pp = excelWorksheet.Cells[9, 5].Value;
+                var x86_sa = excelWorksheet.Cells[14, 5].Value;
+                var x86_sy = excelWorksheet.Cells[15, 5].Value;
+                var x86_oc = excelWorksheet.Cells[16, 5].Value;
+                var x86_cp = excelWorksheet.Cells[18, 5].Value;
+                var x86_itp = excelWorksheet.Cells[19, 5].Value;
+                var x86_op = excelWorksheet.Cells[20, 5].Value;
+                var x86_ir = excelWorksheet.Cells[22, 5].Value;
+                var x86_ftv = excelWorksheet.Cells[23, 5].Value;
+                var x86_te = excelWorksheet.Cells[24, 5].Value;
+                var x86_es = excelWorksheet.Cells[26, 5].Value;
+                var x86_gc = excelWorksheet.Cells[27, 5].Value;
+                var x86_rd = excelWorksheet.Cells[28, 5].Value;
+                var result = new
+                {
+                    //z14_dataProductivityChart =  Convert.ToString(Convert.ToDecimal(z14_cp).ToString("0.##")) + "," + Convert.ToString(Convert.ToDecimal(z14_itp).ToString("0.##")) + "," + Convert.ToString(Convert.ToDecimal(z14_op).ToString("0.##")) ,
+                    //z14_dataCostsChart = Convert.ToString(Convert.ToDecimal(z14_sy).ToString("0.##")) + "," + Convert.ToString(Convert.ToDecimal(z14_sa).ToString("0.##")) + "," + Convert.ToString(Convert.ToDecimal(z14_oc).ToString("0.##")) ,
+                    //z14_dataRevenuesProfitChart =  Convert.ToString(Convert.ToDecimal(z14_ir).ToString("0.##")) + "," + Convert.ToString(Convert.ToDecimal(z14_ftv).ToString("0.##")) + "," + Convert.ToString(Convert.ToDecimal(z14_te).ToString("0.##")) ,
+                    //z14_dataRisksChart = Convert.ToString(Convert.ToDecimal(z14_es).ToString("0.##")) + "," + Convert.ToString(Convert.ToDecimal(z14_gc).ToString("0.##")) + "," + Convert.ToString(Convert.ToDecimal(z14_rd).ToString("0.##")) ,
+
+                    //x86_dataProductivityChart =  Convert.ToString(Convert.ToDecimal(x86_cp).ToString("0.##")) + "," + Convert.ToString(Convert.ToDecimal(x86_itp).ToString("0.##")) + "," + Convert.ToString(Convert.ToDecimal(x86_op).ToString("0.##")) ,
+                    //x86_dataCostsChart =  Convert.ToString(Convert.ToDecimal(x86_sy).ToString("0.##")) + "," + Convert.ToString(Convert.ToDecimal(x86_sa).ToString("0.##")) + "," + Convert.ToString(Convert.ToDecimal(x86_oc).ToString("0.##")) ,
+                    //x86_dataRevenuesProfitChart =  Convert.ToString(Convert.ToDecimal(x86_ir).ToString("0.##")) + "," + Convert.ToString(Convert.ToDecimal(x86_ftv).ToString("0.##")) + "," + Convert.ToString(Convert.ToDecimal(x86_te).ToString("0.##")) ,
+                    //x86_dataRisksChart =  Convert.ToString(Convert.ToDecimal(x86_es).ToString("0.##")) + "," + Convert.ToString(Convert.ToDecimal(x86_gc).ToString("0.##")) + "," + Convert.ToString(Convert.ToDecimal(x86_rd).ToString("0.##")) ,
+
+
+                    z14_dataProductivityChart = Convert.ToString(Convert.ToInt64(z14_cp)) + "," + Convert.ToString(Convert.ToInt64(z14_itp)) + "," + Convert.ToString(Convert.ToInt64(z14_op)),
+                    z14_dataCostsChart = Convert.ToString(Convert.ToInt64(z14_sy)) + "," + Convert.ToString(Convert.ToInt64(z14_sa)) + "," + Convert.ToString(Convert.ToInt64(z14_oc)),
+                    z14_dataRevenuesProfitChart = Convert.ToString(Convert.ToInt64(z14_ir)) + "," + Convert.ToString(Convert.ToInt64(z14_ftv)) + "," + Convert.ToString(Convert.ToInt64(z14_te)),
+                    z14_dataRisksChart = Convert.ToString(Convert.ToInt64(z14_es)) + "," + Convert.ToString(Convert.ToInt64(z14_gc)) + "," + Convert.ToString(Convert.ToInt64(z14_rd)),
+
+                    x86_dataProductivityChart = Convert.ToString(Convert.ToInt64(x86_cp)) + "," + Convert.ToString(Convert.ToInt64(x86_itp)) + "," + Convert.ToString(Convert.ToInt64(x86_op)),
+                    x86_dataCostsChart = Convert.ToString(Convert.ToInt64(x86_sy)) + "," + Convert.ToString(Convert.ToInt64(x86_sa)) + "," + Convert.ToString(Convert.ToInt64(x86_oc)),
+                    x86_dataRevenuesProfitChart = Convert.ToString(Convert.ToInt64(x86_ir)) + "," + Convert.ToString(Convert.ToInt64(x86_ftv)) + "," + Convert.ToString(Convert.ToInt64(x86_te)),
+                    x86_dataRisksChart = Convert.ToString(Convert.ToInt64(x86_es)) + "," + Convert.ToString(Convert.ToInt64(x86_gc)) + "," + Convert.ToString(Convert.ToInt64(x86_rd)),
+
+
+                    dataRoiChart = Convert.ToString(Convert.ToInt64(Convert.ToDecimal(x86_roi)*100)) + "," + Convert.ToString(Convert.ToInt64( Convert.ToDecimal(z14_roi)*100)),
+                    dataPaybackChart = Convert.ToString(Convert.ToDecimal(x86_pp).ToString("0.##")) + "," + Convert.ToString(Convert.ToDecimal(z14_pp).ToString("0.##"))
+
+                };
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+
         }
     }
 }
